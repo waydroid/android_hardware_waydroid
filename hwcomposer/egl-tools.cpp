2d79f7269a9a6e9a9d73d580cfae23e91b578c6b
Alessandro Astone <ales.astone@gmail.com>
2023-01-14 18:44:26 +0100

    hwcomposer: Add EGL logging

--- hwcomposer/egl-tools.cpp
+++ hwcomposer/egl-tools.cpp
@@ -35,6 +35,28 @@
 #include <semaphore.h>
 #include <ui/GraphicBuffer.h>
 
+const char* eglStrError(EGLint err)
+{
+    switch (err){
+        case EGL_SUCCESS:           return "EGL_SUCCESS";
+        case EGL_NOT_INITIALIZED:   return "EGL_NOT_INITIALIZED";
+        case EGL_BAD_ACCESS:        return "EGL_BAD_ACCESS";
+        case EGL_BAD_ALLOC:         return "EGL_BAD_ALLOC";
+        case EGL_BAD_ATTRIBUTE:     return "EGL_BAD_ATTRIBUTE";
+        case EGL_BAD_CONFIG:        return "EGL_BAD_CONFIG";
+        case EGL_BAD_CONTEXT:       return "EGL_BAD_CONTEXT";
+        case EGL_BAD_CURRENT_SURFACE: return "EGL_BAD_CURRENT_SURFACE";
+        case EGL_BAD_DISPLAY:       return "EGL_BAD_DISPLAY";
+        case EGL_BAD_MATCH:         return "EGL_BAD_MATCH";
+        case EGL_BAD_NATIVE_PIXMAP: return "EGL_BAD_NATIVE_PIXMAP";
+        case EGL_BAD_NATIVE_WINDOW: return "EGL_BAD_NATIVE_WINDOW";
+        case EGL_BAD_PARAMETER:     return "EGL_BAD_PARAMETER";
+        case EGL_BAD_SURFACE:       return "EGL_BAD_SURFACE";
+        case EGL_CONTEXT_LOST:      return "EGL_CONTEXT_LOST";
+        default: return "UNKNOWN";
+    }
+}
+
 const char *vertSource =
     "precision mediump float;\n"
     "attribute vec2 in_position;\n"
@@ -136,22 +158,28 @@ void drawQuad(int x, int y, int w, int h) {
 void egl_init(struct display* display) {
     display->egl_dpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
     eglInitialize(display->egl_dpy, NULL, NULL);
+    ALOGI("eglInitialize: %s", eglStrError(eglGetError()));
 
     EGLConfig config;
     int num_config;
     EGLint dpy_attrs[] = { EGL_NATIVE_VISUAL_ID, (EGLint)HAL_PIXEL_FORMAT_RGBA_8888, EGL_SURFACE_TYPE, EGL_PBUFFER_BIT, EGL_NONE };
     eglChooseConfig(display->egl_dpy, dpy_attrs, &config, 1, &num_config);
+    ALOGI("eglChooseConfig: %s", eglStrError(eglGetError()));
 
     EGLint context_attrs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
     EGLContext ctx = eglCreateContext(display->egl_dpy, config,  EGL_NO_CONTEXT, context_attrs);
+    ALOGI("eglCreateContext: %s", eglStrError(eglGetError()));
 
     EGLint pbuf_attrs[] = { EGL_WIDTH, (EGLint)display->width, EGL_HEIGHT, (EGLint)display->height, EGL_NONE };
     EGLSurface pbuf = eglCreatePbufferSurface(display->egl_dpy, config, pbuf_attrs);
+    ALOGI("eglCreatePbufferSurface: %s", eglStrError(eglGetError()));
 
     eglMakeCurrent(display->egl_dpy, pbuf, pbuf, ctx);
+    ALOGI("eglMakeCurrent: %s", eglStrError(eglGetError()));
 
     GLint prog = createProgram(vertSource, fragSource);
     glUseProgram(prog);
+    ALOGI("glUseProgram: %d", glGetError());
 }
 
 void egl_render_to_pixels(struct display* display, struct buffer* buf) {
