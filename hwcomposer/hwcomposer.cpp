aa25045cd57feab89eca24d83fc8e2edd818dadb
Alessandro Astone <ales.astone@gmail.com>
2023-01-23 14:59:35 +0100

    hwcomposer: Use EGL to update shmem buffers from GPU
    Surfaceflinger layers are not created with SW_READ flags so we should
    not be mapping them in main memory.
    It still works for gralloc.default buffers because they're already
    implemented with ashmem.
    
    This code path is used when the we can use GPU buffers but the
    compositor does not implement the dmabuf protocol.

--- hwcomposer/hwcomposer.cpp
+++ hwcomposer/hwcomposer.cpp
@@ -130,9 +130,17 @@ static int hwc_prepare(hwc_composer_device_1_t* dev,
     return 0;
 }
 
-static void update_shm_buffer(struct buffer *buffer)
+static void update_shm_buffer(struct display* display, struct buffer *buffer)
 {
-    // Assume 4bpp formats or none of this is going to work
+    // Slower but always correct
+    if (display->gtype != GRALLOC_DEFAULT) {
+        display->egl_work_queue.push_back(std::bind(egl_render_to_pixels, display, buffer));
+        sem_post(&display->egl_go);
+        sem_wait(&display->egl_done);
+        return;
+    }
+
+    // Fast path for when the buffer is guaranteed to be linear and 4bpp
     void *data;
     int shm_stride, src_stride;
     android::Rect bounds(buffer->width, buffer->height);
@@ -187,7 +195,7 @@ static struct buffer *get_wl_buffer(struct waydroid_hwc_composer_device_1 *pdev,
                 delete (it->second);
                 pdev->display->buffer_map.erase(it);
             } else {
-                update_shm_buffer(it->second);
+                update_shm_buffer(pdev->display, it->second);
                 return it->second;
             }
         } else
@@ -204,7 +212,7 @@ static struct buffer *get_wl_buffer(struct waydroid_hwc_composer_device_1 *pdev,
             ret = create_dmabuf_wl_buffer(pdev->display, buf, drm_handle->width, drm_handle->height, drm_handle->format, -1 /* compute drm format */, drm_handle->prime_fd, pixel_stride, drm_handle->stride, 0 /* offset */, drm_handle->modifier, layer->handle);
         } else {
             ret = create_shm_wl_buffer(pdev->display, buf, drm_handle->width, drm_handle->height, drm_handle->format, pixel_stride, layer->handle);
-            update_shm_buffer(buf);
+            update_shm_buffer(pdev->display, buf);
         }
     } else if (pdev->display->gtype == GRALLOC_CROS) {
         const struct cros_gralloc_handle *cros_handle = (const struct cros_gralloc_handle *)layer->handle;
@@ -212,14 +220,14 @@ static struct buffer *get_wl_buffer(struct waydroid_hwc_composer_device_1 *pdev,
             ret = create_dmabuf_wl_buffer(pdev->display, buf, cros_handle->width, cros_handle->height, cros_handle->droid_format, cros_handle->format, cros_handle->fds[0], pixel_stride, cros_handle->strides[0], cros_handle->offsets[0], cros_handle->format_modifier, layer->handle);
         } else {
             ret = create_shm_wl_buffer(pdev->display, buf, cros_handle->width, cros_handle->height, cros_handle->droid_format, pixel_stride, layer->handle);
-            update_shm_buffer(buf);
+            update_shm_buffer(pdev->display, buf);
         }
     } else {
         if (pdev->display->gtype == GRALLOC_ANDROID) {
             ret = create_android_wl_buffer(pdev->display, buf, width, height, format, pixel_stride, layer->handle);
         } else {
             ret = create_shm_wl_buffer(pdev->display, buf, width, height, format, pixel_stride, layer->handle);
-            update_shm_buffer(buf);
+            update_shm_buffer(pdev->display, buf);
         }
     }
 
