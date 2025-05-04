waydroid/android_hardware_waydroid

commit 1be173002e8373fea09988a1a5c58c9863a1fa9e

hwcomposer: Get window size and scaling information after attaching a buffer
Some compositors like Sway may not send a valid initial configure event until
a buffer is actually attached to the surface.
Attach a dummy buffer to make that work and avoid falling back to using the
whole output size (which is wrong anyways as it's the physical screen size
and not the logical screen size after scaling)

Closes: https://github.com/waydroid/waydroid/issues/1867

hwcomposer/wayland-hwc.cpp
@@ -536,6 +536,16 @@ create_window(struct display *display, bool use_subsurfaces, std::string appID, 
     window->bg_surface = NULL;
     window->bg_subsurface = NULL;
 
+    int fd = syscall(SYS_memfd_create, "buffer", 0);
+    ftruncate(fd, 4);
+    void *shm_data = mmap(NULL, 4, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (shm_data == MAP_FAILED) {
+        ALOGE("mmap failed");
+        close(fd);
+        exit(1);
+    }
+    struct wl_shm_pool *pool = wl_shm_create_pool(display->shm, fd, 4);
+
     bool calibrating = !display->height || !display->width;
 
     if (display->wm_base) {
@@ -583,47 +593,52 @@ create_window(struct display *display, bool use_subsurfaces, std::string appID, 
     }
 
     wl_surface_commit(window->surface);
-
-    if (calibrating && display->fractional_scale_manager) {
-        // We only support one global scale
-        wp_fractional_scale_v1* fs = wp_fractional_scale_manager_v1_get_fractional_scale(
-                display->fractional_scale_manager, window->surface);
-        wp_fractional_scale_v1_add_listener(fs, &fractional_scale_listener, display);
-        wl_display_roundtrip(display->display);
-        wp_fractional_scale_v1_destroy(fs);
-    }
-    finished_computing_scale(display);
-
-    /* Here we retrieve objects if executed without immed, or error */
+    // Handle first configure event
     wl_display_roundtrip(display->display);
-    wl_surface_commit(window->surface);
 
     if (calibrating) {
-        // If we did not receive a window size from the compositor we have to fall back to using the whole output size
-        // At the time of writing this happens on wlroots compositors
+        wp_fractional_scale_v1* fs;
+        if (display->fractional_scale_manager) {
+            // We only support one global scale
+            fs = wp_fractional_scale_manager_v1_get_fractional_scale(
+                    display->fractional_scale_manager, window->surface);
+            wp_fractional_scale_v1_add_listener(fs, &fractional_scale_listener, display);
+        }
+        if (!display->height || !display->width) {
+            // Some compositors fail to give us a window size without a buffer attached
+            // See: https://github.com/swaywm/sway/issues/2176
+            struct wl_buffer *buf = wl_shm_pool_create_buffer(pool, 0, 1, 1, 4, WL_SHM_FORMAT_ARGB8888);
+            wl_surface_attach(window->surface, buf, 0, 0);
+            wl_surface_commit(window->surface);
+            // Try second configure event, with buffer attached
+            wl_display_roundtrip(display->display);
+        }
+        if (display->fractional_scale_manager) {
+            wl_surface_commit(window->surface);
+            wl_display_roundtrip(display->display);
+            wp_fractional_scale_v1_destroy(fs);
+        }
+
+        // If after all of this we still did not receive a proper configure event,
+        // fallback to using the full output size.
+        // NOTICE: full_width and full_heigth should be in compositor logical size!
         if (!display->height)
             display->height = display->full_height / display->scale;
         if (!display->width)
             display->width = display->full_width / display->scale;
+
+        finished_computing_scale(display);
     }
 
     // No subsurface background for us!
     if ((!use_subsurfaces && !display->subcompositor) ||
-        property_get_bool("persist.waydroid.no_background_subsurface", false))
+        property_get_bool("persist.waydroid.no_background_subsurface", false)) {
+        wl_shm_pool_destroy(pool);
         return window;
-
-    int fd = syscall(SYS_memfd_create, "buffer", 0);
-    ftruncate(fd, 4);
-    void *shm_data = mmap(NULL, 4, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (shm_data == MAP_FAILED) {
-        ALOGE("mmap failed");
-        close(fd);
-        exit(1);
     }
+
     uint32_t *buf = (uint32_t*)shm_data;
     *buf = color.a << 24 | color.r << 16 | color.g << 8 | color.b;
-
-    struct wl_shm_pool *pool = wl_shm_create_pool(display->shm, fd, 4);
     window->bg_buffer = wl_shm_pool_create_buffer(pool, 0, 1, 1, 4, WL_SHM_FORMAT_ARGB8888);
     wl_shm_pool_destroy(pool);
     close(fd);
@@ -1367,6 +1382,7 @@ output_handle_mode(void *data, struct wl_output *,
     // Fallback size
     // We can't do anything meaningful if there's more than one display, just pick one at random
     // Hopefully these won't need to be used
+    // NOTICE: Actually, this is wrong when using scaling. We should probably use xdg_output instead.
     d->full_width = width;
     d->full_height = height;
 }

commit f4dc50e7aac24ca524fdbb73fb79c90d2ecb79c3

hwcomposer: Fixup scaling when using user-defined width and height
Fixes: 1be1730 ("hwcomposer: Get window size and scaling information after attaching a buffer")

hwcomposer/hwcomposer.cpp
@@ -1283,9 +1283,6 @@ static int hwc_open(const struct hw_module_t* module, const char* name,
     pthread_mutex_init(&pdev->vsync_lock, NULL);
     pdev->vsync_callback_enabled = true;
 
-    // Initialize width and height with user-provided overrides if any
-    choose_width_height(pdev->display, 0, 0);
-
     auto first_window = create_window(pdev->display, pdev->use_subsurface, "Waydroid", "0", {0, 0, 0, 255});
     if (!property_get_bool("waydroid.background_start", true)) {
         pdev->windows["Waydroid"] = first_window;

hwcomposer/wayland-hwc.cpp
@@ -342,7 +342,7 @@ static const struct xdg_surface_listener xdg_surface_listener = {
 };
 
 
-static void
+void
 finished_computing_scale(struct display *d)
 {
     char property[PROPERTY_VALUE_MAX];
@@ -547,6 +547,10 @@ create_window(struct display *display, bool use_subsurfaces, std::string appID, 
     struct wl_shm_pool *pool = wl_shm_create_pool(display->shm, fd, 4);
 
     bool calibrating = !display->height || !display->width;
+    if (calibrating) {
+        // Initialize width and height with user-provided overrides if any
+        choose_width_height(display, 0, 0);
+    }
 
     if (display->wm_base) {
         window->xdg_surface =

hwcomposer/wayland-hwc.h
@@ -252,5 +252,3 @@ void
 destroy_window(struct window *window, bool keep = false);
 struct window *
 create_window(struct display *display, bool with_dummy, std::string appID, std::string taskID, hwc_color_t color);
-void
-choose_width_height(struct display* display, int32_t hint_width, int32_t hint_height);