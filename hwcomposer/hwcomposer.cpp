waydroid/android_hardware_waydroid

commit 21a2c55890be5b481064644603284ac771ee8e0a

hwcomposer: Use hardware cursor even in full-ui or single-app mode
Use the HWC_IS_CURSOR_LAYER flag to detect the cursor layer and request a
compositionType HWC_OVERLAY so that we can handle the cursor layer
independently and set it as the wayland cursor surface.

This is only now possible because we added support for the cursor hotspot,
otherwise we would be degrading the experience.

hwcomposer/hwcomposer.cpp
@@ -119,6 +119,10 @@ static int hwc_prepare(hwc_composer_device_1_t* dev,
     }
 
     for (size_t i = 0; i < contents->numHwLayers; i++) {
+        if (contents->hwLayers[i].flags & HWC_IS_CURSOR_LAYER) {
+            contents->hwLayers[i].compositionType = HWC_OVERLAY;
+            continue;
+        }
         if (contents->hwLayers[i].compositionType == HWC_FRAMEBUFFER_TARGET)
             continue;
         if (contents->hwLayers[i].flags & HWC_SKIP_LAYER)
@@ -642,6 +646,51 @@ static int hwc_set(struct hwc_composer_device_1* dev,size_t numDisplays,
         }
     }
 
+    // Set Wayland cursor
+    for (size_t l = 0; l < contents->numHwLayers; l++) {
+        hwc_layer_1_t* fb_layer = &contents->hwLayers[l];
+        if ((fb_layer->flags & HWC_IS_CURSOR_LAYER) && pdev->display->cursor_surface) {
+            struct buffer *buf = get_wl_buffer(pdev, fb_layer, l);
+            if (!buf) {
+                ALOGE("Failed to get wayland buffer");
+                if (fb_layer->acquireFenceFd != -1) {
+                    close(fb_layer->acquireFenceFd);
+                }
+                break;
+            }
+
+            wl_surface_attach(pdev->display->cursor_surface, buf->buffer, 0, 0);
+            if (wl_surface_get_version(pdev->display->cursor_surface) >= WL_SURFACE_DAMAGE_BUFFER_SINCE_VERSION)
+                wl_surface_damage_buffer(pdev->display->cursor_surface, 0, 0, buf->width, buf->height);
+            else
+                wl_surface_damage(pdev->display->cursor_surface, 0, 0, buf->width, buf->height);
+            if (!pdev->display->viewporter && pdev->display->scale > 1) {
+                // With no viewporter the scale is guaranteed to be integer
+                wl_surface_set_buffer_scale(pdev->display->cursor_surface, (int)pdev->display->scale);
+            }
+            if (pdev->display->cursor_viewport) {
+                setup_viewport_source(pdev->display->cursor_viewport, fb_layer->sourceCropf, fb_layer->transform);
+                setup_viewport_destination(pdev->display->cursor_viewport, fb_layer->displayFrame, pdev->display);
+            }
+
+            wl_pointer_set_cursor (pdev->display->pointer, pdev->display->pointer_enter_serial,
+                                   pdev->display->cursor_surface,
+                                   roundf(pdev->display->cursor_hotspot.x / pdev->display->scale),
+                                   roundf(pdev->display->cursor_hotspot.y / pdev->display->scale));
+
+            const int kAcquireWarningMS = 100;
+            err = sync_wait(fb_layer->acquireFenceFd, kAcquireWarningMS);
+            if (err < 0 && errno == ETIME) {
+                ALOGE("hwcomposer waited on fence %d for %d ms",
+                    fb_layer->acquireFenceFd, kAcquireWarningMS);
+            }
+            close(fb_layer->acquireFenceFd);
+
+            wl_surface_commit(pdev->display->cursor_surface);
+            break;
+        }
+    }
+
     for (size_t l = 0; l < contents->numHwLayers; l++) {
         hwc_layer_1_t* fb_layer = &contents->hwLayers[l];
         if (fb_layer->compositionType == HWC_FRAMEBUFFER_TARGET) {
@@ -671,6 +720,14 @@ static int hwc_set(struct hwc_composer_device_1* dev,size_t numDisplays,
         hwc_layer_1_t* fb_layer = &contents->hwLayers[layer];
 
         if (fb_layer->flags & HWC_SKIP_LAYER) {
+            if (fb_layer->acquireFenceFd != -1) {
+                close(fb_layer->acquireFenceFd);
+            }
+            continue;
+        }
+
+        if ((fb_layer->flags & HWC_IS_CURSOR_LAYER) && pdev->display->cursor_surface) {
+            // Cursor was already handled separately
             if (fb_layer->acquireFenceFd != -1) {
                 close(fb_layer->acquireFenceFd);
             }
@@ -746,60 +803,23 @@ static int hwc_set(struct hwc_composer_device_1* dev,size_t numDisplays,
             std::string LayerRawName;
             std::istringstream issLayer(layer_name);
             std::getline(issLayer, LayerRawName, '#');
-            if (LayerRawName == "Sprite" && pdev->display->pointer_surface) {
-                // Cursor layer
-                if (pdev->display->cursor_surface) {
-                    struct buffer *buf = get_wl_buffer(pdev, fb_layer, layer);
-                    if (!buf) {
-                        ALOGE("Failed to get wayland buffer");
-                        if (fb_layer->acquireFenceFd != -1) {
-                            close(fb_layer->acquireFenceFd);
+            if ((fb_layer->flags & HWC_IS_CURSOR_LAYER) && !pdev->display->cursor_surface && pdev->display->pointer_surface) {
+                // Cursor layer. Without cursor_surface we draw it as a subsurface
+                for (auto it = pdev->windows.begin(); it != pdev->windows.end(); it++) {
+                    if (it->second) {
+                        if (it->second->surface == pdev->display->pointer_surface) {
+                            window = it->second;
+                            break;
                         }
-                        continue;
-                    }
-
-                    wl_surface_attach(pdev->display->cursor_surface, buf->buffer, 0, 0);
-                    if (wl_surface_get_version(pdev->display->cursor_surface) >= WL_SURFACE_DAMAGE_BUFFER_SINCE_VERSION)
-                        wl_surface_damage_buffer(pdev->display->cursor_surface, 0, 0, buf->width, buf->height);
-                    else
-                        wl_surface_damage(pdev->display->cursor_surface, 0, 0, buf->width, buf->height);
-                    if (!pdev->display->viewporter && pdev->display->scale > 1) {
-                        // With no viewporter the scale is guaranteed to be integer
-                        wl_surface_set_buffer_scale(pdev->display->cursor_surface, (int)pdev->display->scale);
-                    }
-                    if (pdev->display->cursor_viewport) {
-                        setup_viewport_source(pdev->display->cursor_viewport, fb_layer->sourceCropf, fb_layer->transform);
-                        setup_viewport_destination(pdev->display->cursor_viewport, fb_layer->displayFrame, pdev->display);
-                    }
-
-                    wl_pointer_set_cursor (pdev->display->pointer, pdev->display->pointer_enter_serial,
-                                           pdev->display->cursor_surface,
-                                           roundf(pdev->display->cursor_hotspot.x / pdev->display->scale),
-                                           roundf(pdev->display->cursor_hotspot.y / pdev->display->scale));
-                    wl_surface_commit(pdev->display->cursor_surface);
-
-                    if (fb_layer->acquireFenceFd != -1) {
-                        close(fb_layer->acquireFenceFd);
-                    }
-                    continue;
-                } else {
-                    for (auto it = pdev->windows.begin(); it != pdev->windows.end(); it++) {
-                        if (it->second) {
-                            if (it->second->surface == pdev->display->pointer_surface) {
+                        for (auto itt = it->second->surfaces.begin(); itt != it->second->surfaces.end(); itt++) {
+                            if (itt->second == pdev->display->pointer_surface) {
                                 window = it->second;
                                 break;
-                            }
-                            for (auto itt = it->second->surfaces.begin(); itt != it->second->surfaces.end(); itt++) {
-                                if (itt->second == pdev->display->pointer_surface) {
-                                    window = it->second;
-                                    break;
-                                }
                             }
                         }
                     }
                 }
-            }
-            if (LayerRawName == "InputMethod") {
+            } else if (LayerRawName == "InputMethod") {
                 // IME layer
                 if (pdev->windows.find(LayerRawName) == pdev->windows.end()) {
                     pdev->windows[LayerRawName] = create_window(pdev->display, pdev->use_subsurface, LayerRawName, "none", {0, 0, 0, 0});

commit 13b570be1721acc6c804265eecf7406acc12219e

hwcomposer: Invalidate cursor buffer on geometry changes
We were never removing a stale cursor buffer from the map because
we only set display->geo_changed if use_subsurface.

Fixes: 21a2c55 ("hwcomposer: Use hardware cursor even in full-ui or single-app mode")
Closes: https://github.com/waydroid/waydroid/issues/1875

hwcomposer/hwcomposer.cpp
@@ -105,8 +105,15 @@ static int hwc_prepare(hwc_composer_device_1_t* dev,
 
     if (!contents) return 0;
 
-    if ((contents->flags & HWC_GEOMETRY_CHANGED) && pdev->use_subsurface)
-        pdev->display->geo_changed = true;
+    if (contents->flags & HWC_GEOMETRY_CHANGED) {
+        if (pdev->use_subsurface)
+            pdev->display->geo_changed = true;
+        for (auto it = pdev->display->buffer_map.begin(); it != pdev->display->buffer_map.end(); it++) {
+            if (it->second && it->second->may_change_geo) {
+                it->second->invalidated = true;
+            }
+        }
+    }
 
     std::pair<int, int> skipped(-1, -1);
     for (size_t i = 0; i < contents->numHwLayers; i++) {
@@ -220,6 +227,7 @@ static struct buffer *get_wl_buffer(struct waydroid_hwc_composer_device_1 *pdev,
     int ret = 0;
 
     buf = new struct buffer();
+    buf->may_change_geo = pdev->use_subsurface;
     if (pdev->display->gtype == GRALLOC_GBM) {
         struct gralloc_handle_t *drm_handle = (struct gralloc_handle_t *)layer->handle;
         if (pdev->display->dmabuf) {
@@ -447,14 +455,17 @@ static int hwc_set(struct hwc_composer_device_1* dev,size_t numDisplays,
     size_t fb_target = -1;
     int err = 0;
 
-    if (pdev->display->geo_changed) {
-        for (auto it = pdev->display->buffer_map.begin(); it != pdev->display->buffer_map.end(); it++) {
-            if (it->second) {
-                destroy_buffer(it->second);
-            }
+    for (auto it = pdev->display->buffer_map.begin(); it != pdev->display->buffer_map.end(); /* no increment */) {
+        if (it->second && it->second->invalidated == true) {
+            destroy_buffer(it->second);
+            pdev->display->buffer_map.erase(it++);
+        } else {
+            ++it;
         }
-        pdev->display->buffer_map.clear();
     }
+    // Should be redundant, but let's call clear() for good measure...
+    if (pdev->display->geo_changed)
+        pdev->display->buffer_map.clear();
 
     std::pair<int, int> skipped(-1, -1);
     if (pdev->use_subsurface && !pdev->multi_windows) {
@@ -658,6 +669,7 @@ static int hwc_set(struct hwc_composer_device_1* dev,size_t numDisplays,
                 }
                 break;
             }
+            buf->may_change_geo = true;
 
             wl_surface_attach(pdev->display->cursor_surface, buf->buffer, 0, 0);
             if (wl_surface_get_version(pdev->display->cursor_surface) >= WL_SURFACE_DAMAGE_BUFFER_SINCE_VERSION)

hwcomposer/wayland-hwc.h
@@ -191,6 +191,9 @@ struct buffer {
     bool isShm;
     void *shm_data;
     int size;
+
+    bool invalidated;
+    bool may_change_geo;
 };
 
 struct window {